bf10b4bd1eec0fd1bc7dc318653fcb06ab3d8f04
mirelamanthay <166140891+mirelamanthay@users.noreply.github.com>
2026-08-20 09:47:39 -0300

    salve

--- Script.js
+++ Script.js
@@ -12,4 +12,49 @@ const perguntas = [
     enunciado: "Pergunta 2",
     alternativas: ["Alternativa 1", "Alternativa 2"],
   },
+
+
+
+ {
+    enunciado: "Pergunta 1",
+    alternativas: ["Alternativa 1", "Alternativa 2"],
+  },
+  {
+
+    enunciado: "Pergunta 2",
+    alternativas: ["Alternativa 1", "Alternativa 2"],
+  }, 
+  
+  
+  {
+    enunciado: "Pergunta 1",
+    alternativas: ["Alternativa 1", "Alternativa 2"],
+  },
+  {
+
+    enunciado: "Pergunta 2",
+    alternativas: ["Alternativa 1", "Alternativa 2"],
+  }, 
+
+
+  {
+    enunciado: "Pergunta 1",
+    alternativas: ["Alternativa 1", "Alternativa 2"],
+  },
+  {
+
+    enunciado: "Pergunta 2",
+    alternativas: ["Alternativa 1", "Alternativa 2"],
+  }, 
+
+
+  {
+    enunciado: "Pergunta 1",
+    alternativas: ["Alternativa 1", "Alternativa 2"],
+  },
+  {
+
+    enunciado: "Pergunta 2",
+    alternativas: ["Alternativa 1", "Alternativa 2"],
+  }, 
 ];
